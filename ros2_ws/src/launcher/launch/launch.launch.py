fee14dc4900f530c5a25656b07e1aaabda972469
nrf24l01 <s.nevrovsky@ya.ru>
2025-06-14 00:09:07 +0300

    fixed frames

--- ros2_ws/src/launcher/launch/launch.launch.py
+++ ros2_ws/src/launcher/launch/launch.launch.py
@@ -15,21 +15,26 @@ def generate_launch_description():
                 {'state_fetch_interval_sec': 0.5},   # SLOWED DOWN: Was 0.1. Let's try 2 Hz
                 {'request_timeout_sec': 1.0},        # INCREASED: Was 0.5. More patient.
                 {'odom_frame_id': 'odom'},
-                {'base_frame_id': 'base_link'}
+                {'base_frame_id': 'base_footprint'}
             ]
         ),
         # Static Transform Publisher
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_base_link_laser',
-            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser']
-        ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     arguments=['0', '0', '0.1', '0', '0', '0', '1', 'base_link', 'laser']
+        # ),
         # Web Joystick
         Node(
             package='web_joystick',
             executable='web',
             name='web_joystick',
+            output='screen'
+        ),
+        Node(
+            package='lidar_joiner',
+            executable='join',
+            name='lidar_joiner',
             output='screen'
         ),
         # Lidar Reader
