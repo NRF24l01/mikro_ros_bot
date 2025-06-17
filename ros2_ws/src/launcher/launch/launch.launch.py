23e09f57fefa7a0a0ca6de875c11518b5b2bfe40
nrf24l01 <s.nevrovsky@ya.ru>
2025-06-17 10:37:08 +0300

    New day, new ip

--- ros2_ws/src/launcher/launch/launch.launch.py
+++ ros2_ws/src/launcher/launch/launch.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
             output='screen',
             emulate_tty=True,
             parameters=[
-                {'esp_ip_address': '192.168.0.109'}, # Using the IP from your log
+                {'esp_ip_address': '192.168.0.110'}, # Using the IP from your log
                 {'state_fetch_interval_sec': 0.5},   # SLOWED DOWN: Was 0.1. Let's try 2 Hz
                 {'request_timeout_sec': 1.0},        # INCREASED: Was 0.5. More patient.
                 {'odom_frame_id': 'sts_odom'},
@@ -39,13 +39,13 @@ def generate_launch_description():
             name='lidar_reader',
             output='screen',
             parameters=[
-                {'host': "192.168.0.109"},
+                {'host': "192.168.0.110"},
                 {'port': 8888},
                 {'frame_id': "sts_laser"},
                 {'range_min': 0.05},
                 {'range_max': 16.0},
                 {'lidar_xyz': [0.0, 0.0, 0.1]},
-                {'lidar_rpy_deg': [0.0, 0.0, 180.0]},
+                {'lidar_rpy_deg': [0.0, 0.0, -180.0]},
                 {'base_frame': "sts_base_frame"},
             ]
         ),
